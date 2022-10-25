dc656d6909592b7e52046bfe9926e67ab5a3bf39
espilya <espilya@gmail.com>
2022-10-25 21:38:15 +0200

    removed update CM and flags. also updated openapi spec

--- api-server/app/controllers/communities.js
+++ api-server/app/controllers/communities.js
@@ -5,26 +5,26 @@ var post = require('./postUpdateCM');
 var jobManager = require('./jobsRoute/jobsManager.js');
 
 module.exports.getCommunities = function getCommunities(req, res, next) {
-  Flags.getFlags()
+  // Flags.getFlags()
+  //   .then(function (response) {
+  //     if (response == null) {
+  Communities.getCommunities()
     .then(function (response) {
-      if (response == null) {
-        Communities.getCommunities()
-          .then(function (response) {
-            res.status(200).send(response);
-          })
-          .catch(function (response) {
-            res.status(400).send(response);
-          });
-      }
-      else {
-        post.update_CM("allPerspectives");
-        var data = jobManager.createJob(0, "getPerspectives")
-        res.status(202).send(data);
-      }
+      res.status(200).send(response);
     })
     .catch(function (response) {
-      console.error("Communities.getCommunities -> Flags.getFlags: error: " + response)
+      res.status(400).send(response);
     });
+  //   }
+  //   else {
+  //     post.update_CM("allPerspectives");
+  //     var data = jobManager.createJob(0, "getPerspectives")
+  //     res.status(202).send(data);
+  //   }
+  // })
+  // .catch(function (response) {
+  //   console.error("Communities.getCommunities -> Flags.getFlags: error: " + response)
+  // });
 };
 
 module.exports.getCommunityById = function getCommunityById(req, res, next) {
@@ -32,21 +32,21 @@ module.exports.getCommunityById = function getCommunityById(req, res, next) {
   Communities.getCommunityById(communityId)
     .then(function (response) {
       var community = response
-      console.log(community.perspectiveId)
-      Flags.getFlagsById(community.perspectiveId)
-        .then(function (flag) {
-          if (flag == null) { // flag does not exist => no update needed
-            res.status(200).send(community);
-          }
-          else { //flag exist
-            post.update_CM(community.perspectiveI);
-            var data = jobManager.createJob(communityId, "getCommunityById")
-            res.status(202).send(data);
-          }
-        })
-        .catch(function (response) {
-          console.error("Communities.getCommunityById -> Flags.getFlagsById: error: " + response)
-        });
+      // console.log(community.perspectiveId)
+      // Flags.getFlagsById(community.perspectiveId)
+      //   .then(function (flag) {
+      // if (flag == null) { // flag does not exist => no update needed
+      res.status(200).send(community);
+      // }
+      // else { //flag exist
+      //   post.update_CM(community.perspectiveI);
+      //   var data = jobManager.createJob(communityId, "getCommunityById")
+      //   res.status(202).send(data);
+      // }
+      //     })
+      //     .catch(function (response) {
+      //       console.error("Communities.getCommunityById -> Flags.getFlagsById: error: " + response)
+      //     });
     })
     .catch(function (response) {
       res.status(400).send("invalid community id");
@@ -55,29 +55,29 @@ module.exports.getCommunityById = function getCommunityById(req, res, next) {
 
 module.exports.listCommunityUsers = function listCommunityUsers(req, res, next) {
   const communityId = req.params[idParam];
-  //get users list
+  //get users list from communityId community
   Communities.listCommunityUsers(communityId)
     .then(function (response) {
       var users = response;
       //get community
-      Communities.getCommunityById(communityId)
-        .then(function (response) {
-          var community = response;
-          Flags.getFlagsById(community.perspectiveId)
-            .then(function (flag) {
-              if (flag == null) { // flag does not exist => no update needed
-                res.status(200).send(users);
-              }
-              else { //flag exist
-                post.update_CM(community.perspectiveId);
-                var data = jobManager.createJob(communityId, "listCommunityUsers")
-                res.status(202).send(data);
-              }
-            })
-            .catch(function (response) {
-              console.error("Communities.listCommunityUsers -> Flags.getFlagsById: error: " + response)
-            });
-        })
+      // Communities.getCommunityById(communityId)
+      //   .then(function (response) {
+      //     var community = response;
+      //     Flags.getFlagsById(community.perspectiveId)
+      //       .then(function (flag) {
+      //         if (flag == null) { // flag does not exist => no update needed
+      res.status(200).send(users);
+      //       }
+      //       else { //flag exist
+      //         post.update_CM(community.perspectiveId);
+      //         var data = jobManager.createJob(communityId, "listCommunityUsers")
+      //         res.status(202).send(data);
+      //       }
+      //     })
+      //     .catch(function (response) {
+      //       console.error("Communities.listCommunityUsers -> Flags.getFlagsById: error: " + response)
+      //     });
+      // })
     })
     .catch(function (response) {
       res.status(400).send("invalid community id");
--- api-server/app/controllers/perspectives.js
+++ api-server/app/controllers/perspectives.js
@@ -8,26 +8,26 @@ var jobManager = require('./jobsRoute/jobsManager.js');
 // if exist then update CM
 // if !exist then access DB
 module.exports.getPerspectives = function getPerspectives(req, res, next) {
-  Flags.getFlags()
+  // Flags.getFlags()
+  //   .then(function (response) {
+  //     if (response == null) {
+  Perspectives.getPerspectives()
     .then(function (response) {
-      if (response == null) {
-        Perspectives.getPerspectives()
-          .then(function (response) {
-            res.status(200).send(response);
-          })
-          .catch(function (response) {
-            res.status(400).send("invalid perspective id");
-          });
-      }
-      else {
-        post.update_CM("allPerspectives");
-        var data = jobManager.createJob(0, "getPerspectives")
-        res.status(202).send(data);
-      }
+      res.status(200).send(response);
     })
     .catch(function (response) {
-      console.error("Perspectives.getPerspectives -> Flags.getFlags: error: " + response)
+      res.status(400).send("invalid perspective id");
     });
+  //   }
+  //   else {
+  //     post.update_CM("allPerspectives");
+  //     var data = jobManager.createJob(0, "getPerspectives")
+  //     res.status(202).send(data);
+  //   }
+  // })
+  // .catch(function (response) {
+  //   console.error("Perspectives.getPerspectives -> Flags.getFlags: error: " + response)
+  // });
 };
 
 // check if flag exist
@@ -35,26 +35,26 @@ module.exports.getPerspectives = function getPerspectives(req, res, next) {
 // if !exist then access DB
 module.exports.getPerspectiveById = function getPerspectiveById(req, res, next) {
   const perspectiveId = req.params[idParam];
-  Flags.getFlagsById(perspectiveId)
+  // Flags.getFlagsById(perspectiveId)
+  //   .then(function (response) {
+  //     if (response == null) {
+  Perspectives.getPerspectiveById(perspectiveId)
     .then(function (response) {
-      if (response == null) {
-        Perspectives.getPerspectiveById(perspectiveId)
-          .then(function (response) {
-            res.status(200).send(response);
-          })
-          .catch(function (response) {
-            res.status(400).send("invalid perspective id");
-          });
-      }
-      else {
-        post.update_CM(perspectiveId);
-        var data = jobManager.createJob(0, "getPerspectives")
-        res.status(202).send(data);
-      }
+      res.status(200).send(response);
     })
     .catch(function (response) {
-      console.error("Perspectives.getPerspectiveById -> Flags.getFlagsById: error: " + response)
+      res.status(400).send("invalid perspective id");
     });
+  //   }
+  //   else {
+  //     post.update_CM(perspectiveId);
+  //     var data = jobManager.createJob(0, "getPerspectives")
+  //     res.status(202).send(data);
+  //   }
+  // })
+  // .catch(function (response) {
+  //   console.error("Perspectives.getPerspectiveById -> Flags.getFlagsById: error: " + response)
+  // });
 };
 
 module.exports.listPerspectiveCommunities = function listPerspectiveCommunities(req, res, next) {
@@ -62,40 +62,40 @@ module.exports.listPerspectiveCommunities = function listPerspectiveCommunities(
 
   // Check flag, if exist then access mongodb and return data
   // if false then check if perspetive exist, create new job and return 202, and a link to that job
-  Flags.getFlagsById(perspectiveId)
+  // Flags.getFlagsById(perspectiveId)
+  //   .then(function (response) {
+  //     if (response == null) { // flag does not exist => no update needed
+  Perspectives.listPerspectiveCommunities(perspectiveId)
     .then(function (response) {
-      if (response == null) { // flag does not exist => no update needed
-        Perspectives.listPerspectiveCommunities(perspectiveId)
-          .then(function (response) {
-            res.status(200).send(response);
-          })
-          .catch(function (response) {
-            res.status(400).send("invalid perspective id");
-          });
-      }
-      else { //flag exist
-        post.update_CM(perspectiveId);
-        var data = jobManager.createJob(perspectiveId, "listPerspectiveCommunities")
-        res.status(202).send(data);
-      }
+      res.status(200).send(response);
     })
     .catch(function (response) {
-      console.error("Perspectives.listPerspectiveCommunities -> Flags.getFlagsById: error: " + response)
+      res.status(400).send("invalid perspective id");
     });
+  //   }
+  //   else { //flag exist
+  //     post.update_CM(perspectiveId);
+  //     var data = jobManager.createJob(perspectiveId, "listPerspectiveCommunities")
+  //     res.status(202).send(data);
+  //   }
+  // })
+  // .catch(function (response) {
+  //   console.error("Perspectives.listPerspectiveCommunities -> Flags.getFlagsById: error: " + response)
+  // });
 };
 
 // redirect post request to api_loader
 module.exports.PostPerspective = function PostPerspective(req, res, next) {
   try {
     Perspectives.PostPerspective(req.body)
-    .then(function (response) {
-      res.status(204);
-      res.send(response);
-    })
-    .catch(function (response) {
-      res.status(501);
-      res.send(response);
-    });
+      .then(function (response) {
+        res.status(204);
+        res.send(response);
+      })
+      .catch(function (response) {
+        res.status(501);
+        res.send(response);
+      });
   } catch (error) {
     console.error(error)
   }
